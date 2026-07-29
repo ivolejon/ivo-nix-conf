54f645d77ae68f3aebc37ca23216df035959ea97
Ivo Lejon <ivo.lejon@gmail.com>
2026-07-29 21:29:34 +0200

    fix: remove starship to use custom prompt with git_branch_name

--- home.nix
+++ home.nix
@@ -34,19 +34,6 @@ in
     shellAliases = {};
   };
 
-  programs.starship = {
-    enable = true;
-    settings = {
-      add_newline = false;
-      format = "$directory$git_branch$git_status$cmd_duration$line_break$character";
-      character = {
-        success_symbol = "[❯](purple)";
-        error_symbol = "[❯](red)";
-      };
-      cmd_duration.format = "[$duration]($style) ";
-    };
-  };
-
   # Edit-in-place: the real file stays in my repo, ~/.config just points at it.
   home.file.".config/nvim".source =
     config.lib.file.mkOutOfStoreSymlink "${dotfiles}/home/.config/nvim";
